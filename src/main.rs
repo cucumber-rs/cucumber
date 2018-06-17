dc9d83baa26a1ae5cc3caefd3c2e232c72e344ec
Brendan Molloy <brendan@bbqsrc.net>
2018-06-17 18:58:28 +0200

    Refactor most of content outside of macro

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,236 @@
 pub extern crate gherkin;
 extern crate regex;
 
+use gherkin::{Step, StepType, Feature};
+use regex::Regex;
+use std::collections::HashMap;
+use std::fs::{self, File};
+use std::hash::{Hash, Hasher};
+use std::io::prelude::*;
+use std::ops::Deref;
+use std::panic;
+use std::path::Path;
+use std::sync::Mutex;
+
+struct HashableRegex(Regex);
+impl Hash for HashableRegex {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.0.as_str().hash(state);
+    }
+}
+
+impl PartialEq for HashableRegex {
+    fn eq(&self, other: &HashableRegex) -> bool {
+        self.0.as_str() == other.0.as_str()
+    }
+}
+
+impl Eq for HashableRegex {}
+
+impl Deref for HashableRegex {
+    type Target = Regex;
+
+    fn deref(&self) -> &Regex {
+        &self.0
+    }
+}
+
+struct TestCase<T: Default> {
+    pub test: fn(&mut T) -> ()
+}
+
+impl<T: Default> TestCase<T> {
+    fn new(test: fn(&mut T) -> ()) -> TestCase<T> {
+        TestCase {
+            test: test
+        }
+    }
+}
+
+struct RegexTestCase<T: Default> {
+    pub test: fn(&mut T, &[String]) -> ()
+}
+
+impl<T: Default> RegexTestCase<T> {
+    fn new(test: fn(&mut T, &[String]) -> ()) -> RegexTestCase<T> {
+        RegexTestCase {
+            test: test
+        }
+    }
+}
+
+struct CucumberTests<T: Default> {
+    pub given: HashMap<&'static str, TestCase<T>>,
+    pub when: HashMap<&'static str, TestCase<T>>,
+    pub then: HashMap<&'static str, TestCase<T>>,
+    pub regex: CucumberRegexTests<T>
+}
+
+struct CucumberRegexTests<T: Default> {
+    pub given: HashMap<HashableRegex, RegexTestCase<T>>,
+    pub when: HashMap<HashableRegex, RegexTestCase<T>>,
+    pub then: HashMap<HashableRegex, RegexTestCase<T>>,
+}
+
+enum TestType<'a, T> where T: 'a, T: Default {
+    Normal(&'a TestCase<T>),
+    Regex(&'a RegexTestCase<T>, Vec<String>)
+}
+
+impl<T: Default> CucumberTests<T> {
+    fn new() -> CucumberTests<T> {
+        let regex_tests = CucumberRegexTests {
+            given: HashMap::new(),
+            when: HashMap::new(),
+            then: HashMap::new()
+        };
+
+        let tests = CucumberTests {
+            given: HashMap::new(),
+            when: HashMap::new(),
+            then: HashMap::new(),
+            regex: regex_tests
+        };
+
+        tests
+    }
+
+    fn test_type<'a>(&'a self, step: &Step, value: &str) -> Option<TestType<'a, T>> {
+        let test_bag = match step.ty {
+            StepType::Given => &self.given,
+            StepType::When => &self.when,
+            StepType::Then => &self.then
+        };
+
+        match test_bag.get(value) {
+            Some(v) => Some(TestType::Normal(v)),
+            None => {
+                let regex_bag = match step.ty {
+                    StepType::Given => &self.regex.given,
+                    StepType::When => &self.regex.when,
+                    StepType::Then => &self.regex.then
+                };
+
+                let result = regex_bag.iter()
+                    .find(|(regex, _)| regex.is_match(&value));
+
+                match result {
+                    Some((regex, tc)) => {
+                        let thing = regex.0.captures(&value).unwrap();
+                        let matches: Vec<String> = thing.iter().map(|x| x.unwrap().as_str().to_string()).collect();
+                        Some(TestType::Regex(tc, matches))
+                    },
+                    None => {
+                        None
+                    }
+                }
+            }
+        }
+    }
+
+    pub fn run(&mut self, feature_path: &Path) {
+        use std::sync::Arc;
+
+        let last_panic: Arc<Mutex<Option<String>>> = Arc::new(Mutex::new(None));
+        let last_panic_hook = last_panic.clone();
+
+        panic::set_hook(Box::new(move |info| {
+            let mut state = last_panic_hook.lock().expect("last_panic unpoisoned");
+            *state = info.location().map(|x| format!("{}:{}:{}", x.file(), x.line(), x.column()));
+        }));
+
+        let feature_path = fs::read_dir(feature_path).expect("feature path to exist");
+
+        let mut scenarios = 0;
+        let mut steps = 0;
+
+        for entry in feature_path {
+            let mut file = File::open(entry.unwrap().path()).expect("file to open");
+            let mut buffer = String::new();
+            file.read_to_string(&mut buffer).unwrap();
+            let feature = Feature::from(&*buffer);
+            
+            println!("Feature: {}\n", feature.name);
+
+            for scenario in feature.scenarios {
+                scenarios += 1;
+
+                println!("  Scenario: {}", scenario.name);
+
+                let mut world = Mutex::new(T::default());
+
+                for step in scenario.steps {
+                    steps += 1;
+
+                    let value = step.value.to_string();
+                    
+                    let test_type = match self.test_type(&step, &value) {
+                        Some(v) => v,
+                        None => {
+                            println!("    {}\n      # No test found", &step.to_string());
+                            continue;
+                        }
+                    };
+
+                    let result = panic::catch_unwind(|| {
+                        match world.lock() {
+                            Ok(mut world) => {
+                                match test_type {
+                                    TestType::Normal(t) => (t.test)(&mut *world),
+                                    TestType::Regex(t, c) => (t.test)(&mut *world, &c)
+                                }
+                            },
+                            Err(e) => {
+                                return Err(e);
+                            }
+                        };
+
+                        return Ok(())
+                    });
+
+                    println!("    {:<40}", &step.to_string());
+
+                    match result {
+                        Ok(inner) => {
+                            match inner {
+                                Ok(_) => {},
+                                Err(_) => println!("      # Skipped due to previous error")
+                            }
+                        }
+                        Err(any) => {
+                            println!("      # Step failed:");
+                            let mut state = last_panic.lock().expect("unpoisoned");
+
+                            {
+                                let loc = match &*state {
+                                    Some(v) => &v,
+                                    None => "unknown"
+                                };
+
+                                if let Some(s) = any.downcast_ref::<String>() {
+                                    println!("      # {}  [{}]", &s, loc);
+                                } else if let Some(s) = any.downcast_ref::<&str>() {
+                                    println!("      # {}  [{}]", &s, loc);
+                                }
+                            }
+
+                            *state = None;
+                        }
+                    };
+                }
+
+                println!("");
+            }
+        }
+
+        let _ = panic::take_hook();
+
+        println!("# Scenarios: {}", scenarios);
+        println!("# Steps: {}", steps);
+    }
+}
+
+
 macro_rules! cucumber {
     (
         @gather_steps, $worldtype:ident, $tests:tt,
@@ -39,264 +269,21 @@ macro_rules! cucumber {
     };
 
     (
-        $featurepath:tt; $worldtype:ident; $( $items:tt )*
+        features: $featurepath:tt;
+        world: $worldtype:ident;
+        $( $items:tt )*
     ) => {
-        mod cucumber_tests {
-            use regex::{Regex, Captures};
-            use super::$worldtype;
-            use std::fs::{self, File};
-            use std::collections::HashMap;
-            use $crate::gherkin::{Feature, StepType, Step};
-            use std::io::prelude::*;
-            use std::panic;
-            use std::sync::Mutex;
-
-            struct TestCase {
-                pub test: fn(&mut $worldtype) -> ()
-            }
-
-            impl TestCase {
-                fn new(test: fn(&mut $worldtype) -> ()) -> TestCase {
-                    TestCase {
-                        test: test
-                    }
-                }
-            }
-
-            struct RegexTestCase {
-                pub test: fn(&mut $worldtype, &[String]) -> ()
-            }
-
-            impl RegexTestCase {
-                fn new(test: fn(&mut $worldtype, &[String]) -> ()) -> RegexTestCase {
-                    RegexTestCase {
-                        test: test
-                    }
-                }
-            }
-
-            use std::hash::{Hash, Hasher};
-            use std::ops::Deref;
-
-            struct HashableRegex(Regex);
-            impl Hash for HashableRegex {
-                fn hash<H: Hasher>(&self, state: &mut H) {
-                    self.0.as_str().hash(state);
-                }
-            }
-
-            impl PartialEq for HashableRegex {
-                fn eq(&self, other: &HashableRegex) -> bool {
-                    self.0.as_str() == other.0.as_str()
-                }
-            }
-
-            impl Eq for HashableRegex {}
-
-            impl Deref for HashableRegex {
-                type Target = Regex;
-
-                fn deref(&self) -> &Regex {
-                    &self.0
-                }
-            }
-
-            struct CucumberTests {
-                pub given: HashMap<&'static str, TestCase>,
-                pub when: HashMap<&'static str, TestCase>,
-                pub then: HashMap<&'static str, TestCase>,
-                pub regex: CucumberRegexTests
-            }
-
-            struct CucumberRegexTests {
-                pub given: HashMap<HashableRegex, RegexTestCase>,
-                pub when: HashMap<HashableRegex, RegexTestCase>,
-                pub then: HashMap<HashableRegex, RegexTestCase>,
-            }
-
-            enum TestType<'a> {
-                Normal(&'a TestCase),
-                Regex(&'a RegexTestCase, Vec<String>)
-            }
-
-            impl CucumberTests {
-                #[allow(unused_assignments)]
-                #[allow(unused_mut)]
-                fn new() -> CucumberTests {
-                    let mut given = HashMap::new();
-                    let mut when = HashMap::new();
-                    let mut then = HashMap::new();
-
-                    let mut given_regex = HashMap::new();
-                    let mut when_regex = HashMap::new();
-                    let mut then_regex = HashMap::new();
-
-                    let mut regex_tests = CucumberRegexTests {
-                        given: given_regex,
-                        when: when_regex,
-                        then: then_regex
-                    };
-
-                    let mut tests = CucumberTests {
-                        given: given,
-                        when: when,
-                        then: then,
-                        regex: regex_tests
-                    };
-
-                    cucumber!(@gather_steps, $worldtype, tests, $( $items )*);
-
-                    tests
-                }
-
-                fn test_type<'a>(&'a self, step: &Step, value: &str) -> Option<TestType<'a>> {
-                    let test_bag = match step.ty {
-                        StepType::Given => &self.given,
-                        StepType::When => &self.when,
-                        StepType::Then => &self.then
-                    };
-
-                    match test_bag.get(value) {
-                        Some(v) => Some(TestType::Normal(v)),
-                        None => {
-                            let regex_bag = match step.ty {
-                                StepType::Given => &self.regex.given,
-                                StepType::When => &self.regex.when,
-                                StepType::Then => &self.regex.then
-                            };
-
-                            let result = regex_bag.iter()
-                                .find(|(regex, _)| regex.is_match(&value));
-
-                            match result {
-                                Some((regex, tc)) => {
-                                    let thing = regex.0.captures(&value).unwrap();
-                                    let matches: Vec<String> = thing.iter().map(|x| x.unwrap().as_str().to_string()).collect();
-                                    Some(TestType::Regex(tc, matches))
-                                },
-                                None => {
-                                    None
-                                }
-                            }
-                        }
-                    }
-                }
-
-                pub fn run(&mut self) {
-                    use std::sync::Arc;
-
-                    let last_panic: Arc<Mutex<Option<String>>> = Arc::new(Mutex::new(None));
-                    let last_panic_hook = last_panic.clone();
-
-                    panic::set_hook(Box::new(move |info| {
-                        // println!("{:?}", info.location());
-                        let mut state = last_panic_hook.lock().expect("last_panic unpoisoned");
-                        *state = info.location().map(|x| format!("{}:{}:{}", x.file(), x.line(), x.column()));
-                    }));
-
-                    let feature_path = fs::read_dir($featurepath).expect("feature path to exist");
-
-                    let mut scenarios = 0;
-                    let mut steps = 0;
-
-                    for entry in feature_path {
-                        let mut file = File::open(entry.unwrap().path()).expect("file to open");
-                        let mut buffer = String::new();
-                        file.read_to_string(&mut buffer).unwrap();
-                        let feature = Feature::from(&*buffer);
-                        
-                        println!("Feature: {}\n", feature.name);
-
-                        for scenario in feature.scenarios {
-                            scenarios += 1;
-
-                            println!("  Scenario: {}", scenario.name);
-
-                            let mut world = Mutex::new($worldtype::default());
-
-                            for step in scenario.steps {
-                                steps += 1;
-
-                                let value = step.value.to_string();
-                                
-                                let test_type = match self.test_type(&step, &value) {
-                                    Some(v) => v,
-                                    None => {
-                                        println!("    {}\n      # No test found", &step.to_string());
-                                        continue;
-                                    }
-                                };
-
-                                let result = panic::catch_unwind(|| {
-                                    match world.lock() {
-                                        Ok(mut world) => {
-                                            match test_type {
-                                                TestType::Normal(t) => (t.test)(&mut *world),
-                                                TestType::Regex(t, c) => (t.test)(&mut *world, &c)
-                                            }
-                                        },
-                                        Err(e) => {
-                                            return Err(e);
-                                        }
-                                    };
-
-                                    return Ok(())
-                                });
-
-                                println!("    {:<40}", &step.to_string());
-
-                                match result {
-                                    Ok(inner) => {
-                                        match inner {
-                                            Ok(_) => {},
-                                            Err(_) => println!("      # Skipped due to previous error")
-                                        }
-                                    }
-                                    Err(any) => {
-                                        println!("      # Step failed:");
-                                        let mut state = last_panic.lock().expect("unpoisoned");
-
-                                        {
-                                            let loc = match &*state {
-                                                Some(v) => &v,
-                                                None => "unknown"
-                                            };
-
-                                            if let Some(s) = any.downcast_ref::<String>() {
-                                                println!("      # {}  [{}]", &s, loc);
-                                            } else if let Some(s) = any.downcast_ref::<&str>() {
-                                                println!("      # {}  [{}]", &s, loc);
-                                            }
-                                        }
-
-                                        *state = None;
-
-                                        // break;
-                                    }
-                                };
-                            }
-
-                            println!("");
-                        }
-                    }
-
-                    let _ = panic::take_hook();
-
-                    println!("# Scenarios: {}", scenarios);
-                    println!("# Steps: {}", steps);
-                }
-            }
-
-            pub fn run() {
-                CucumberTests::new().run()
-            }
-        }
-        
         fn main() {
-            cucumber_tests::run();
+            use regex::Regex;
+            use std::path::Path;
+            use $crate::{CucumberTests, TestCase, RegexTestCase, HashableRegex};
+            use $worldtype;
+
+            let mut tests: CucumberTests<$worldtype> = CucumberTests::new();
+            cucumber!(@gather_steps, $worldtype, tests, $( $items )*);
+            tests.run(Path::new($featurepath));
         }
     };
-
 }
 
 use std::default::Default;
@@ -313,26 +300,27 @@ impl Default for World {
     }
 }
 
-cucumber! { "./features"; World;
+cucumber! {
+    features: "./features";
+    world: World;
 
-when regex "^test (.*) regex$" |world, matches| {
-    println!("{}", matches[1]);
-};
+    when regex "^test (.*) regex$" |world, matches| {
+        println!("{}", matches[1]);
+    };
 
-given "a thing" |world| {
-    assert!(true);
-};
+    given "a thing" |world| {
+        assert!(true);
+    };
 
-when "another thing" |world| {
-    assert!(true);
-};
+    when "another thing" |world| {
+        assert!(true);
+    };
 
-when "something goes right" |world| { 
-    assert!(true);
-};
+    when "something goes right" |world| { 
+        assert!(true);
+    };
 
-then "another thing" |world| {
-    assert!(true)
-};
-
+    then "another thing" |world| {
+        assert!(true)
+    };
 }
