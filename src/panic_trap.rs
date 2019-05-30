4dac01201a9e872a46df9f2ab42369554f6ae4a1
Adam Reichold <adam.reichold@rhebo.com>
2019-05-30 18:41:05 +0200

    Avoid to unwrap while we have a custom panic hook set.

--- src/panic_trap.rs
+++ src/panic_trap.rs
@@ -13,10 +13,9 @@ pub struct PanicDetails {
 
 impl PanicDetails {
     fn from_panic_info(info: &panic::PanicInfo) -> PanicDetails {
-        let info_payload = info.payload();
-        let payload = if let Some(s) = info_payload.downcast_ref::<String>() {
+        let payload = if let Some(s) = info.payload().downcast_ref::<String>() {
             s.clone()
-        } else if let Some(s) = info_payload.downcast_ref::<&str>() {
+        } else if let Some(s) = info.payload().downcast_ref::<&str>() {
             s.deref().to_owned()
         } else {
             "Opaque panic payload".to_owned()
@@ -25,7 +24,7 @@ impl PanicDetails {
         let location = info
             .location()
             .map(|loc| format!("{}:{}:{}", loc.file(), loc.line(), loc.column()))
-            .unwrap_or_else(|| "Unknown location".to_owned());
+            .unwrap_or_else(|| "Unknown panic location".to_owned());
 
         PanicDetails { payload, location }
     }
@@ -62,25 +61,26 @@ impl<T> PanicTrap<T> {
         let last_panic = Arc::new(Mutex::new(None));
 
         panic::set_hook({
-            let last_panic_hook = last_panic.clone();
+            let last_panic = last_panic.clone();
+
             Box::new(move |info| {
-                let mut state = last_panic_hook.lock().expect("last_panic unpoisoned");
-                *state = Some(PanicDetails::from_panic_info(info));
+                *last_panic.lock().expect("Last panic mutex poisoned") =
+                    Some(PanicDetails::from_panic_info(info));
             })
         });
 
-        let result = panic::catch_unwind(panic::AssertUnwindSafe(f)).map_err(|_| {
-            last_panic
-                .lock()
-                .expect("Last panic mutex poisoned")
-                .clone()
-                .expect("Panic occurred but no panic details were set")
-        });
+        let result = panic::catch_unwind(panic::AssertUnwindSafe(f));
 
-        let _ = panic::take_hook();
+        panic::take_hook();
 
         PanicTrap {
-            result,
+            result: result.map_err(|_| {
+                last_panic
+                    .lock()
+                    .expect("Last panic mutex poisoned")
+                    .take()
+                    .expect("Panic occurred but no panic details were set")
+            }),
             stdout: Vec::new(),
             stderr: Vec::new(),
         }
