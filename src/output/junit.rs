534300753daeef475dc51947b60b631d11425789
Tymoteusz Paul <puciek@gmail.com>
2020-05-27 20:02:54 +0100

    A bit of refactoring and making clippy happy

--- src/output/junit.rs
+++ src/output/junit.rs
@@ -11,10 +11,9 @@ use crate::event::{FeatureEvent, ScenarioEvent, StepEvent, CucumberEvent};
 use crate::{
     EventHandler,
 };
-use gherkin::{Feature, Rule, Scenario, Step};
+use gherkin::{Scenario, Step};
 use std::io::stdout;
 use std::rc::Rc;
-use std::borrow::Borrow;
 
 pub struct JunitOutput {
     report: Report,
@@ -31,13 +30,36 @@ impl Default for JunitOutput {
 }
 
 impl JunitOutput {
-    fn handle_step(&mut self, step: Rc<Step>, event: StepEvent) {
+    fn handle_step(&mut self, step: Rc<Step>, _event: StepEvent) {
         let test_case = TestCase::success(
             step.value.as_ref(),
             Duration::seconds(5),
             Some(step.value.to_string()),
         );
         self.test_suite.add_testcase(test_case);
+    }
+
+    fn handle_scenario(&mut self, scenario: Rc<Scenario>, event: ScenarioEvent) {
+        match event {
+            ScenarioEvent::Starting => {
+                self.test_suite = TestSuite::new(scenario.name.as_ref());
+            },
+            ScenarioEvent::Background(step, event) => {
+                self.handle_step(step, event);
+            },
+            ScenarioEvent::Step(step, event) => {
+                self.handle_step(step, event);
+            },
+            ScenarioEvent::Skipped => {
+                self.report.add_testsuite(self.test_suite.clone());
+            },
+            ScenarioEvent::Passed => {
+                self.report.add_testsuite(self.test_suite.clone());
+            },
+            ScenarioEvent::Failed => {
+                self.report.add_testsuite(self.test_suite.clone());
+            },
+        }
     }
 }
 
@@ -46,36 +68,15 @@ impl EventHandler for JunitOutput {
         match event {
             CucumberEvent::Starting => {
             }
-            CucumberEvent::Feature(feature, event) => match event {
+            CucumberEvent::Feature(_feature, event) => match event {
                 FeatureEvent::Starting => {
                     self.report = Report::new();
                 }
                 FeatureEvent::Scenario(scenario, event) => {
-                    match event {
-                        ScenarioEvent::Starting => {
-                            self.test_suite = TestSuite::new(scenario.name.as_ref());
-                        },
-                        ScenarioEvent::Background(step, event) => {
-                            self.handle_step(step, event);
-                        },
-                        ScenarioEvent::Step(step, event) => {
-                            self.handle_step(step, event);
-                        },
-                        ScenarioEvent::Skipped => {
-                            self.report.add_testsuite(self.test_suite.clone());
-                        },
-                        ScenarioEvent::Passed => {
-                            self.report.add_testsuite(self.test_suite.clone());
-                        },
-                        ScenarioEvent::Failed => {
-                            self.report.add_testsuite(self.test_suite.clone());
-                        },
-                    }
+                    self.handle_scenario(scenario, event)
                 }
                 FeatureEvent::Rule(_rule, _event) => {}
-                FeatureEvent::Finished => {
-                    // self.report.add_testsuite(self.test_suite.clone());
-                }
+                FeatureEvent::Finished => {}
             },
             CucumberEvent::Finished => {
                 self.report.write_xml(stdout()).unwrap();
