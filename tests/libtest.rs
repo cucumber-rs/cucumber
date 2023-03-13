ad3551161cdd11963e2f738bcba59832a96f6bf0
ilslv <ilya.solovyiov@gmail.com>
2023-03-13 11:26:51 +0100

    Support `--report-time` CLI argument

--- tests/libtest.rs
+++ tests/libtest.rs
@@ -1,6 +1,6 @@
 use std::{fs, io::Read as _};
 
-use cucumber::{given, then, when, writer, World as _, WriterExt as _};
+use cucumber::{cli, given, then, when, writer, World as _, WriterExt as _};
 use regex::Regex;
 use tempfile::NamedTempFile;
 
@@ -32,7 +32,7 @@ async fn output() {
     // Required to strip out non-deterministic parts of output, so we could
     // compare them well.
     let non_deterministic = Regex::new(
-        "\"exec_time\":\\d*\\.?\\d*\
+        "\":\\d+\\.\\d+\
          |([^\"\\n\\s]*)[/\\\\]([A-z1-9-_]*)\\.(feature|rs)(:\\d+:\\d+)?\
          |\\s?\n",
     )
@@ -42,6 +42,45 @@ async fn output() {
         non_deterministic.replace_all(&buffer, ""),
         non_deterministic.replace_all(
             &fs::read_to_string("tests/libtest/correct.stdout").unwrap(),
+            "",
+        ),
+    );
+}
+
+#[tokio::test]
+async fn output_report_time() {
+    let mut cli = cli::Opts::<_, _, writer::libtest::Cli>::default();
+    cli.writer.report_time = Some(writer::libtest::ReportTime::Plain);
+    let mut file = NamedTempFile::new().unwrap();
+    drop(
+        World::cucumber()
+            .with_writer(
+                writer::Libtest::new(file.reopen().unwrap()).normalized(),
+            )
+            .fail_on_skipped()
+            .with_cli(cli)
+            .run("tests/features/wait")
+            .await,
+    );
+
+    let mut buffer = String::new();
+    file.read_to_string(&mut buffer).unwrap();
+
+    // Required to strip out non-deterministic parts of output, so we could
+    // compare them well.
+    // let non_deterministic = Regex::new(
+    let non_deterministic = Regex::new(
+        "\":\\d+\\.\\d+\
+         |([^\"\\n\\s]*)[/\\\\]([A-z1-9-_]*)\\.(feature|rs)(:\\d+:\\d+)?\
+         |\\s?\n",
+    )
+    .unwrap();
+
+    assert_eq!(
+        non_deterministic.replace_all(&buffer, ""),
+        non_deterministic.replace_all(
+            &fs::read_to_string("tests/libtest/correct-report-time.stdout")
+                .unwrap(),
             "",
         ),
     );
