eb685d0369ad01484ac477a110c0294f9f5bb6bf
50U10FCA7 <m.bashilov.srg@gmail.com>
2023-09-22 08:54:53 -0500

    Make tests output deterministic [run ci]

--- tests/output.rs
+++ tests/output.rs
@@ -59,20 +59,73 @@ static SPAN_OR_PATH_RE: &Lazy<Regex> = regex!(
 
 #[cfg(test)]
 mod spec {
-    use std::fs;
+    use std::{fmt, fs, io};
 
     use cucumber::{
         writer::{self, Coloring},
         World as _, WriterExt as _,
     };
     use globwalk::GlobWalkerBuilder;
+    use lazy_regex::regex;
+    use once_cell::sync::Lazy;
+    use regex::Regex;
 
     use super::{DebugWriter, World};
 
-    fn load_file(path: impl AsRef<str>) -> Vec<u8> {
+    /// [`Regex`] to make `cargo careful` assertion output match `cargo test`
+    /// output.
+    static CAREFUL_ASSERTION: &Lazy<Regex> = regex!(
+        "assertion `left == right` failed:\
+         \n(.*)\
+         \n\tleft: (.*)\
+         \n\tright: (.*)"
+    );
+
+    /// Format to replace [`CAREFUL_ASSERTION`] with.
+    const STD_ASSERTION: &str = "\
+        assertion failed: `(left == right)`:\
+        \n$1\
+        \n\tleft: `$2`,\
+        \n\tright: `$3`\
+    ";
+
+    /// [`Regex`] to transform full paths to relative paths.
+    static FULL_PATH: &Lazy<Regex> =
+        regex!("/(.*)/tests/features/output/(\\S+).feature");
+
+    /// Format to replace [`FULL_PATH`] with.
+    const RELATIVE_PATH: &str = "tests/features/output/$2.feature";
+
+    /// Deterministic output of [`writer::Basic`].
+    #[derive(Clone, Debug, Default)]
+    struct Output(Vec<u8>);
+
+    impl io::Write for Output {
+        fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+            self.0.extend_from_slice(buf);
+            Ok(buf.len())
+        }
+
+        fn flush(&mut self) -> io::Result<()> {
+            Ok(())
+        }
+    }
+
+    impl fmt::Display for Output {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            let o = String::from_utf8(self.0.clone())
+                .unwrap_or_else(|e| panic!("`Output` is not a string: {e}"));
+            let o = CAREFUL_ASSERTION.replace_all(&o, STD_ASSERTION);
+            let o = FULL_PATH.replace_all(&o, RELATIVE_PATH);
+            write!(f, "{o}")
+        }
+    }
+
+    /// Loads a file from the file system as a string.
+    fn load_file(path: impl AsRef<str>) -> String {
         let path = path.as_ref();
-        fs::read(path)
-            .unwrap_or_else(|_| panic!("Failed to load `{path}` file"))
+        fs::read_to_string(path)
+            .unwrap_or_else(|e| panic!("Failed to load `{path}` file: {e}"))
     }
 
     #[tokio::test]
@@ -101,39 +154,39 @@ mod spec {
                 .with_default_cli()
                 .run(format!("tests/features/output/{file}"))
                 .await;
-            assert_eq!(
-                debug.output.clone().into_bytes(),
-                expected,
-                "\n[debug] file: {file}"
-            );
+            assert_eq!(expected, debug.output, "\n[debug] file: {file}");
 
             let expected =
                 load_file(format!("tests/features/output/{file}.basic.out"));
-            let mut actual = Vec::new();
+            let mut output = Output::default();
             _ = World::cucumber()
                 .with_writer(
-                    writer::Basic::raw(&mut actual, Coloring::Never, 0)
+                    writer::Basic::raw(&mut output, Coloring::Never, 0)
                         .discard_stats_writes()
                         .normalized(),
                 )
                 .with_default_cli()
                 .run(format!("tests/features/output/{file}"))
                 .await;
-            assert_eq!(actual, expected, "\n[basic] file: {file}");
+            assert_eq!(expected, output.to_string(), "\n[basic] file: {file}");
 
             let expected =
                 load_file(format!("tests/features/output/{file}.colored.out"));
-            let mut actual = Vec::new();
+            let mut output = Output::default();
             _ = World::cucumber()
                 .with_writer(
-                    writer::Basic::raw(&mut actual, Coloring::Always, 0)
+                    writer::Basic::raw(&mut output, Coloring::Always, 0)
                         .discard_stats_writes()
                         .normalized(),
                 )
                 .with_default_cli()
                 .run(format!("tests/features/output/{file}"))
                 .await;
-            assert_eq!(actual, expected, "\n[colored] file: {file}");
+            assert_eq!(
+                expected,
+                output.to_string(),
+                "\n[colored] file: {file}"
+            );
         }
     }
 }
